0e500c3ab1030c265b1d4097f62e47aa524b6d24
Helen <helen@Mac.local>
2022-10-18 23:52:12 +0700

    practical tasks 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max)
+    Console.WriteLine($"max = {num2}");
+
+else
+    Console.WriteLine($"max = {num1}");
+
