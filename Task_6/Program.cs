633021df352c914524df889a96e3ac2eb87f0959
Helen <helen@Mac.local>
2022-10-21 12:21:21 +0700

    practical tasks 6

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-int num = new Random().Next(100, 999);
-Console.WriteLine(num);
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
 
 int k = num / 10 % 10;
 
