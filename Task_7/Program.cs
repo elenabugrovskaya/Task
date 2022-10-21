14f1d5ae5af8c9dc0ef36cafe1c61b2c15db58db
Helen <helen@Mac.local>
2022-10-21 13:21:52 +0700

    practical tasks 7

--- Task_7/Program.cs
+++ Task_7/Program.cs
@@ -1,2 +1,11 @@
-﻿// 
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int num = new Random().Next();
+Console.WriteLine(num);
+
+if (num > 99) Console.WriteLine($"Третья цифра заданного числа {num.ToString()[2]}");
+else  Console.WriteLine("Третьей цифры нет"); 
+    
+       
+
+
