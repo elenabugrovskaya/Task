776fffa25104941dfe9a9b3615eab8ef37a94e62
Helen <helen@Mac.local>
2022-10-23 19:23:39 +0700

    practical tasks 12

---
+++ Task_12/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Таблица кубов чисел от 1 до {n}:");
+
+for (int i=1; i<=n; i++)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+}
+
