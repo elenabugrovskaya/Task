f8a0d95caac0881771994759733df210c5b585c0
Helen <helen@Mac.local>
2022-10-25 16:21:47 +0700

    practical tasks 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Создать программу, которая из массива удаляется элемент с позиции, которую введет пользователь.
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 14, 185, 12, 32, 34, 14, 82, 34, 15, 0, 92 };
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.Write(" {0}", x[i]);
+            }
+            Console.WriteLine("\nВведите номер того элемента,который нужно удалить 0-10:");
+            int c = int.Parse(Console.ReadLine());
+            List<int> temp = x.ToList();
+            temp.RemoveAt(c);
+            x = temp.ToArray();
+ 
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.Write(" {0}", x[i]);
+            }
+ 
+        }
+    }
---
+++ Task_14/Program.cs
@@ -0,0 +1,25 @@
+﻿// Создание массива из двух массивов. В итоговом массиве должны быть элементы первого и второго массива.
+// [1,2,3] [4,5,6] -> [1,2,3,4,5,6]
+
+using System;
+public class Example
+{
+    public static void Main()
+{
+    int[] arr1 = { 1, 2, 3};
+    Console.WriteLine(String.Join(" ", arr1));
+    int[] arr2 = {4, 5, 6};
+    Console.WriteLine(String.Join(" ", arr2));
+    int[] result = new int[arr1.Length + arr2.Length];
+    Array.Copy(arr1, result, arr1.Length);
+    Array.Copy(arr2, 0, result, arr1.Length, arr2.Length);
+    Console.WriteLine("Итоговый массив:");
+    Console.WriteLine(String.Join(" ", result));
+}
+}
+
+
+
+
+
+           
