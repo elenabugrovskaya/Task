08ea3c393e5d03d70736c7158cadbd0822fc020b
Helen <helen@Mac.local>
2022-10-29 13:49:48 +0700

    Task_16

--- Task_16/Program.cs
+++ Task_16/Program.cs
@@ -1,7 +1,11 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
+//452 -> 11
+//82 -> 10
+//9012 -> 12
 Console.Write("Введите число от 1 до 9999: ");
 int num = int.Parse(Console.ReadLine());
+void summa()
+{
 int sum = 0;
 if (num < 100)
    sum = num/10 + num%10;
@@ -9,5 +13,6 @@ else if (num<1000)
    sum = num/100%10 + num/10%10 + num%10;
 else if (num<=9999)
    sum = num/1000%10 + num/100%10 + num/10%10 + num%10;
-
 Console.WriteLine($"Сумма цифр в числе: {sum}");
+}
+summa();
