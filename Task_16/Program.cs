88e7d0f807af8d6bc44818457310ccb99ca25e5e
Helen <helen@Mac.local>
2022-10-30 13:49:37 +0700

    Task_16

--- Task_16/Program.cs
+++ Task_16/Program.cs
@@ -2,17 +2,19 @@
 //452 -> 11
 //82 -> 10
 //9012 -> 12
-Console.Write("Введите число от 1 до 9999: ");
+
+Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
 void summa()
 {
-int sum = 0;
-if (num < 100)
-   sum = num/10 + num%10;
-else if (num<1000)
-   sum = num/100%10 + num/10%10 + num%10;
-else if (num<=9999)
-   sum = num/1000%10 + num/100%10 + num/10%10 + num%10;
+            int sum = 0;
+            while (num > 0)
+            {
+ 
+                sum = sum + num % 10;
+                num = num /10 ;
+ 
+            }
 Console.WriteLine($"Сумма цифр в числе: {sum}");
 }
 summa();
---
+++ Task_19/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+void summa()
+{
+            int sum = 0;
+            while (num > 0)
+            {
+ 
+                sum = sum + num % 10;
+                num = num /10 ;
+ 
+            }
+Console.WriteLine($"Сумма цифр в числе: {sum}");
+}
+summa();
+
+Console.Write("Введите число от 1 до 9999: ");
+int num = int.Parse(Console.ReadLine());
+void summa()
+{
+int sum = 0;
+if (num < 100)
+   sum = num/10 + num%10;
+else if (num<1000)
+   sum = num/100%10 + num/10%10 + num%10;
+else if (num<=9999)
+   sum = num/1000%10 + num/100%10 + num/10%10 + num%10;
+Console.WriteLine($"Сумма цифр в числе: {sum}");
+}
+summa();
