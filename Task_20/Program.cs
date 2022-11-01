58176262404d58553acc71aa05c08d9efa31a760
Helen <helen@Mac.local>
2022-11-01 23:02:24 +0700

    Task_20

--- Task_20/Program.cs
+++ Task_20/Program.cs
@@ -2,31 +2,8 @@
 // на доске, определите, есть ли среди них пара бьющих друг друга. Программа получает на вход восемь пар чисел, каждое число
 // от 1 до 8 — координаты 8 ферзей. Если ферзи не бьют друг друга, выведите слово NO, иначе выведите YES.
 
+/*
 int n = 2;
-//List<int> numbers = new List<int> ();
-//Console.WriteLine("Введите координаты 8 ферзей: ");
-/*void Koordinati()
-        {
-            List<int> list = new List<int>(2);
-            //for (int i = 0; i < 2; i++)
-            //{
-                int x = int.Parse(Console.ReadLine());
-                int y = int.Parse(Console.ReadLine());
-                list.Add(x);
-                list.Add(y);
-
-            //}
-            Console.WriteLine(" ");  
-            Console.WriteLine(" ");   
-            //for (int i = 0; i < 8; i++)
-
-
-        }
-for (int i=1; i<=2; i++)
-Koordinati();
-*/
-//int[] x = {7, 2, 8, 4, 5, 1, 6, 3};
-//int[] y = {8, 2, 5, 3, 7, 4, 1, 6};
 int[] CreateMas (int n)
 {
     int[] arr = new int[n];
@@ -60,9 +37,11 @@ void CreateList(List<int> numb, int[] arr)
     numb.Add(arr[1]);
     }
 }
+*/
 
-
-
+int n = 8;
+int[] x = {7, 2, 8, 4, 5, 1, 6, 3};
+int[] y = {8, 2, 5, 3, 7, 4, 1, 6};
 bool correct = true;
 for (int i=0; i<n; i++)
     {
