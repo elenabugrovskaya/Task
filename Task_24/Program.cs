54571e33074fd8d284f8aa3a6f8cf4ee80527428
Helen <helen@Mac.local>
2022-11-01 16:20:18 +0700

    Task_24

--- Task_24/Program.cs
+++ Task_24/Program.cs
@@ -1,30 +1,31 @@
 ﻿// Дан список некоторых целых чисел, найдите значение 20 в нем и, если оно присутствует, замените его на 200. Обновите список
 // только при первом вхождении числа 20.
 // [21, 53,20, 56, 45,20] -> [21, 53,200, 56, 45,20]
-int[] CreateMas (int n)
+
+void CreateList(List<int> numb)
 {
-    int[] arr = new int[n];
-    for (int i=0; i<n; i++)
-    arr[i] = new Random().Next(-99, 99);
-    return arr; 
-} 
-void Print(int[] arr)
+    for (int i=0; i<10; i++)
+    numb.Add(new Random().Next(19, 30));
+    numb.Add(20);
+}
+void Print(List<int> numb)
 {
-    Console.WriteLine(String.Join(" ", arr));
+    Console.WriteLine(String.Join(" ", numb));
     Console.Write("\n");
 }
-int sum = 0;
-int ColNeChet (int[] arr)
+void PrintEnd(List<int> numb)
 {
+    for(int i=0; i<12; i++)
     {
-     for (int i=0; i<arr.Length; i++)
-     if (arr[i]%2 != 0) sum = sum +arr[i];
+    if (numb[i]==20) numb[i]=200;
+    break;
     }
-return sum;
+    Console.WriteLine(String.Join(" ", numb));
+    Console.Write("\n");
 }
-int[] array = new int[5];
-array = CreateMas(5);
-Print(array);
-ColNeChet(array);
-Console.WriteLine($"Сумма нечетных элементов массива: {sum}");
 
+List<int> numbers = new List<int> ();
+numbers.Add(20);
+CreateList(numbers);
+Print (numbers);
+PrintEnd(numbers);
