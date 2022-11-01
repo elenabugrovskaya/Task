2c89d74df5f138f91536c7305300d23053dfcad4
Helen <helen@Mac.local>
2022-11-01 13:13:54 +0700

    Task_21

--- Task_20/Program.cs
+++ Task_20/Program.cs
@@ -1,40 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-#include <iostream>
-#include <vector>
-#include <cmath>
-using namespace std;
+﻿// Известно, что на доске 8×8 можно расставить 8 ферзей так, чтобы они не били друг друга. Вам дана расстановка 8 ферзей на
+// доске, определите, есть ли среди них пара бьющих друг друга.
+// Программа получает на вход восемь пар чисел, каждое число от 1 до 8 — координаты 8 ферзей. Если ферзи не бьют друг друга,
+// выведите слово NO, иначе выведите YES.
 
 int main() {
-    vector<int> vertical(8);
-    vector<int> gorizontal(8);
-    1 7
-2 4
-3 2
-4 8
-5 6
-6 1
-7 3
-8 5
-
-    int x[1]=1; int y[1]=7; 
-
-    int b = 0;
-
-    for (int i = 0; i < 8; i++) {
-        if (x[i] == x[i + 1])
-            b += 1;
-        else if (y[i] == y[i + 1])
-            b += 1;
-        else if (abs(y[i] - y[i + 1]) == abs(y[i + 1] - y[i]))
-            b += 1;
+    int temp;
+    bool cross = false;
+    vector <int> x, y;
+    for (int i = 1; i <= 8; i++) {
+        int temp = int.Parse(Console.ReadLine());
+        x.push_back(temp);
+        int temp = int.Parse(Console.ReadLine());
+        y.push_back(temp);
     }
-
-    if (b != 0)
-        Console.WriteLine("YES");
-    else
-        Console.WriteLine("NO");
-
+    for (int i = 0; i < x.size() - 1; i++) {
+        for (int j = i + 1; j < x.size(); j++) {
+            if (Math.Abs((x[i] - x[j]) == Math.Abs((y[i] - y[j]) || x[i] == x[j] || y[i] == y[j]) 
+            {
+                cross = true;
+            }
+        }
+    }
+    if (cross) Console.WriteLine("YES");
+    else Console.WriteLine("NO");
     return 0;
 }
-
---
+++ Task_21/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateMas (int n)
+{
+    int[] arr = new int[n];
+    for (int i=0; i<n; i++)
+    arr[i] = new Random().Next(100, 999);
+    return arr; 
+} 
+void Print(int[] arr)
+{
+    Console.WriteLine(String.Join(" ", arr));
+    Console.Write("\n");
+}
+int k = 0;
+int ColChet (int[] arr)
+{
+    {
+     for (int i=0; i<arr.Length; i++)
+     if (arr[i]%2 == 0) k++;
+    }
+return k;
+}
+int[] array = new int[5];
+array = CreateMas(5);
+Print(array);
+ColChet(array);
+Console.WriteLine($"Количество четных чисел в массиве: {k}");
+
+
+
+
