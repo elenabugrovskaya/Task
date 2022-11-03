c8d3f8637648172917f13dbf6148895f40e56aac
Helen <helen@Mac.local>
2022-11-03 17:17:04 +0700

    Task_26

---
+++ Task_26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+void Peresech()
+{
+if (k1 == k2)  Console.WriteLine("Прямые параллельны");
+else if (k1*k2 == -1) Console.WriteLine("Прямые перпендикулярны");
+else if (k1/k2 == b1/b2) Console.WriteLine("Прямые совпадают");
+else Console.WriteLine($"Точка пересечения прямых({(b1-b2)/(k2-k1)}, {k1*((b1-b2)/(k2-k1))+b1})");
+}
+Peresech();
