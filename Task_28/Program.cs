af98918db6a82c221b9d8ff3aca10f62ddc1beaa
Helen <helen@Mac.local>
2022-11-05 04:38:24 +0700

    Task_28

--- Task_28/Program.cs
+++ Task_28/Program.cs
@@ -9,15 +9,13 @@ for (int i=0; i<m.GetLength(0); i++)
     for(int j=0; j<m.GetLength(1); j++)
     {
         if (i==j) m[i,j]=i+1; 
-        //if (i!=j) m[i,j]=i+1;
-        //if (j==0 || i!=j) m[i,j]=i+1;
-         if (j<i)
+        if (j<i)
          {
              cc=j+1;
              m[i,j]=cc;
          }
          m[j,i]=m[i,j];
-          if (i<j)
+        if (i<j)
          {
              cc=i+1;
              m[i,j]=cc;
