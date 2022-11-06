1293cf35509028c89a1027a5d66dbaa0be1c2050
Helen <helen@Mac.local>
2022-11-06 20:20:23 +0700

    Task_29

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -1,12 +1,16 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
+// m = 3, n = 3. 
+// Пример:
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
 
 void FillList(List<List<double>> mylist)
 {
-    for(int i=0; i<5; i++)
+    for(int i=0; i<3; i++)
     {
         mylist.Add(new List<double>());
-        for(int j=0; j<5; j++)
+        for(int j=0; j<3; j++)
         mylist[i].Add(new Random().NextDouble()*5);
     }
 }
