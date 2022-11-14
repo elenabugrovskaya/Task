22dadff74c044b8e7ed9f0ee5dd913f982ad2975
Helen <helen@Mac.local>
2022-11-14 13:18:08 +0700

    Task_36

---
+++ Task_36/Program.cs
@@ -0,0 +1,113 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+/* void FillList(List<List<int>> mylist)
+{
+    Console.WriteLine("Задайте рамер массива: ");
+    int n = int.Parse(Console.ReadLine());
+    int m = int.Parse(Console.ReadLine());
+    for(int i=0; i<n; i++)
+    {
+        mylist.Add(new List<int>());
+        for(int j=0; j<m; j++)
+        mylist[i].Add(new Random().Next(0,10));
+    }
+}
+void PrintList(List<List<int>> mylist)
+{
+    for(int i=0; i<mylist.Count; i++)
+    {
+        for(int j=0; j<mylist.Count; j++)
+        Console.Write(mylist[i][j] + "\t");
+        Console.WriteLine();
+    }
+}
+static double[][] MatrixProduct(double[][] matrixA,
+  double[][] matrixB)
+{
+  int aRows = matrixA.Length; int aCols = matrixA[0].Length;
+  int bRows = matrixB.Length; int bCols = matrixB[0].Length;
+  if (aCols != bRows)
+    throw new Exception("Non-conformable matrices in MatrixProduct");
+  double[][] result = MatrixCreate(aRows, bCols);
+  for (int i = 0; i < aRows; ++i) // каждая строка A
+    for (int j = 0; j < bCols; ++j) // каждый столбец B
+      for (int k = 0; k < aCols; ++k)
+        result[i][j] += matrixA[i][k] * matrixB[k][j];
+  return result;
+}
+Console.WriteLine("Hello, World!");
+ */
+
+ using System;
+ 
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность первой матрицы: ");
+            int[,] A = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    Console.Write("A[{0},{1}] = ", i, j);
+                    A[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Введите размерность второй матрицы: ");
+            int[,] B = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < B.GetLength(0); i++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    Console.Write("B[{0},{1}] = ", i, j);
+                    B[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+ 
+            Console.WriteLine("\nМатрица A:");
+            Print(A);
+            Console.WriteLine("\nМатрица B:");
+            Print(B);
+            Console.WriteLine("\nМатрица C = A * B:");
+            int[,] C = Multiplication(A, B);
+            Print(C);
+ 
+        }
+        static int[,] Multiplication(int[,] a, int[,] b)
+        {
+            if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
+            int[,] r = new int[a.GetLength(0), b.GetLength(1)];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    for (int k = 0; k < b.GetLength(0); k++)
+                    {
+                        r[i,j] += a[i,k] * b[k,j];
+                    }
+                }
+            }
+            return r;
+        }
+        static void Print(int[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", a[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
