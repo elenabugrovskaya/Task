1bc04a24056faf4f05a074e4f73b193e8d2de5ec
Helen <helen@Mac.local>
2022-11-14 17:10:47 +0700

    Task_38

---
+++ Task_38/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4. 
+
+void Spiral (int[,] arr)
+{
+Console.WriteLine("Введите количество элементов в строке"); 
+int n = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите количество элементов в cтолбце"); 
+int m = int.Parse(Console.ReadLine()); 
+int[,] mas = new int[m, n]; 
+int k = 1; 
+int t = 0; 
+int i, j = 0; 
+int n1 = n; 
+int m1 = m; 
+ 
+while (k <= n1 * m1) 
+{ 
+for (i = t; i < n; i++) 
+mas[j, i] = k++; 
+j = n - 1; 
+for (i = t + 1; i < m; i++) 
+mas[i, j] = k++; 
+j = m - 1; 
+for (i = n - 2; i >= t; i--) 
+mas[j, i] = k++; 
+j = t; 
+for (i = m - 2; i > t; i--) 
+mas[i, j] = k++; 
+n--; 
+m--; 
+t++; 
+j = t; 
+} 
+for (i = 0; i < mas.GetLength(0); i++) 
+{ 
+for (j = 0; j < mas.GetLength(1); j++) 
+Console.Write("{0,2} ", mas[i, j]); 
+Console.WriteLine(); 
+} 
+     
+}
+int[,] array = new int[4, 4];
+Spiral(array); 
+
+ 
