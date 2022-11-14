173f276af5b5abe7b11095f0e8ab9dfa30fb4ec7
Helen <helen@Mac.local>
2022-11-14 16:11:35 +0700

    Task_35

--- Task_35/Program.cs
+++ Task_35/Program.cs
@@ -6,4 +6,44 @@
 5 2 6 7 
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка */
 
-Console.WriteLine("Hello, World!");
+void FillMas (int[,] arr)
+{
+    for (int i=0; i<arr.GetLength(0); i++)
+    for (int j=0; j<arr.GetLength(1); j++)
+    arr[i, j] = new Random().Next(2, 10);
+}
+void Output (int [,] arr)
+{
+    for (int i=0; i<arr.GetLength(0); i++)
+    {
+        for (int j=0; j<arr.GetLength(1); j++)
+        Console.Write(arr[i, j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+} 
+void Average(int [,] arr)
+{ 
+ int minRowSum = int.MaxValue, indexMinRow = 0;
+ 
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    int rowSum = 0;
+    for (int j = 0; j < arr.GetLength(1); j++)
+    rowSum += arr[i, j];
+                
+    if (rowSum < minRowSum)
+    {
+        minRowSum = rowSum;
+        indexMinRow = i;
+    }
+}
+Console.WriteLine("Строка с минимальной суммой элементов:");
+for(int j = 0;j<arr.GetLength(1);j++)
+Console.Write(arr[indexMinRow, j] + "\t");    
+}  
+
+int[,] array = new int [4, 5];
+FillMas(array);
+Output(array);
+Average(array);
