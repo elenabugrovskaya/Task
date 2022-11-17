e11d2eebbabf1010ef813ffbfd04fb683d703069
Helen <helen@Mac.local>
2022-11-17 13:55:07 +0700

    Task_41

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 
+int Akkerman (int n, int m)
+{
+    if (n == 0) return m + 1;
+    else
+    if ((n != 0) && (m == 0)) return Akkerman(n - 1, 1);
+    else return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+Console.WriteLine(Akkerman(3, 2));
